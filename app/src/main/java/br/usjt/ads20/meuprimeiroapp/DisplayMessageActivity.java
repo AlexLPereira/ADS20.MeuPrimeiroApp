bfd18415c739da4333876a1af91899846c3968b3
AlexLPereira <58451305+AlexLPereira@users.noreply.github.com>
2020-08-21 22:24:48 -0300

    Initial commit

---
+++ app/src/main/java/br/usjt/ads20/meuprimeiroapp/DisplayMessageActivity.java
@@ -0,0 +1,28 @@
+package br.usjt.ads20.meuprimeiroapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import static android.widget.TextView.*;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40.0f);
+        textView.setText(message);
+
+        ViewGroup layout = findViewById(R.id.activity_display_message);
+        layout.addView(textView);
+    }
+}
